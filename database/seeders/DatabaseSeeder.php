bdf700ed84755a432bc4c88c9b7d2c56b9edf57a
fahrezapratamahidayat <fahrezaxgamer@gmail.com>
2025-01-14 22:00:10 +0700

    Increase user factory creation from 5 to 20 in DatabaseSeeder for enhanced test data generation.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,6 @@ class DatabaseSeeder extends Seeder
             'role' => 'admin',
         ]);
 
-        \App\Models\User::factory(5)->create();
+        \App\Models\User::factory(20)->create();
     }
 }
